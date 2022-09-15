40b8f36fac11c39d51e1aeb82011c19cc99e8654
aryan.gupta2 <aryan.gupta2@DEL1-LHP-N82120>
2022-09-15 13:50:11 +0530

    Add project files.

---
+++ CInjection/Program.cs
@@ -0,0 +1,43 @@
+﻿public interface IClass
+{
+    void Serve();
+}
+public class Resource1: IClass
+{
+    public void Serve()
+    {
+        Console.WriteLine("Resource is Initialized");
+    }
+}
+public class Resource2: IClass
+{
+    public void Serve()
+    {
+        Console.WriteLine("Resource2 is Initialized");
+    }
+}
+public class Client
+{
+    private IClass _service;
+    public Client(IClass service)
+    {
+        _service = service;
+    }
+    public void Serve()
+    {
+        _service.Serve();
+    }
+}
+class Program
+{
+    static void Main(String[] args)
+    {
+        Resource1 r1=new Resource1();   
+        Client cA=new Client(r1);
+        cA.Serve();
+
+        Resource2 r2=new Resource2();
+        cA=new Client(r2);
+        cA.Serve();
+    }
+}
